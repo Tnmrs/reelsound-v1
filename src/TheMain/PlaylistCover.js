8016bde40d05b7af16fc3cbe36770ab659ced416
tnmrs <promomyers@gmail.com>
2022-10-01 14:59:03 +0300

    context menu

---
+++ src/TheMain/PlaylistCover.js
@@ -0,0 +1,11 @@
+function PlaylistCover() {
+  return (
+    <img
+      src="https://fakeimg.pl/600/7f1d1d/fff?text=Coverfont=lobster"
+      className="rounded shadow-lg"
+      alt=""
+    />
+  );
+}
+
+export default PlaylistCover;
---
+++ src/TheMain/PlaylistDescription.js
@@ -0,0 +1,5 @@
+function PlaylistDescription() {
+  return <p className="text-sm text-[#b3b3b3] line-clamp-2">1</p>;
+}
+
+export default PlaylistDescription;
