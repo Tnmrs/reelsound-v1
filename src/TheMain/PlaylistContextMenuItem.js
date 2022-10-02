a68f0f5ede0b5775429efc1369adc2e61447277d
tnmrs <promomyers@gmail.com>
2022-10-02 20:25:44 +0300

    playlist menu

--- src/TheMain/PlaylistContextMenuItem.js
+++ src/TheMain/PlaylistContextMenuItem.js
@@ -1,9 +1,25 @@
-function PlaylistContextMenuItem({ children: label }) {
+import PlaylistContextSubmenu from './PlaylistContextSubmenu';
+import { ChevronRightIcon } from '@heroicons/react/outline';
+
+function PlaylistContextMenuItem({ children: label, subMenuItems }) {
+  let classes = '';
+  let buttonClasses = 'w-full p-3 text-left hover:text-white hover:bg-[#3e3e3e] cursor-default';
+  let icon = null;
+  let submenu = null;
+
+  if (subMenuItems) {
+    classes = 'relative';
+    buttonClasses = `${buttonClasses} flex justify-between items-center`;
+    icon = <ChevronRightIcon className="h-4 w-4" />;
+
+    submenu = <PlaylistContextSubmenu />;
+  }
   return (
-    <li>
-      <button className="w-full p-3 text-left hover:text-white hover:bg-[#3e3e3e] cursor-default">
-        {label}
+    <li className={classes}>
+      <button className={buttonClasses}>
+        {label} {icon}
       </button>
+      {submenu}
     </li>
   );
 }
--- src/TheMain/PlaylistContextSubmenu.js
+++ src/TheMain/PlaylistContextSubmenu.js
@@ -1,15 +1,5 @@
 import PlaylistContextMenuItem from './PlaylistContextMenuItem';
 
-const subMenuItems = [
-  {
-    label: 'Copy link to playlist',
-  },
-
-  {
-    label: 'Embed playlist',
-  },
-];
-
 function PlaylistContextSubmenu() {
   return (
     <ul className="absolute top-0 left-full bg-[#282828] text-[#eaeaea] text-sm p-1 rounded shadow-xl cursor-default">
