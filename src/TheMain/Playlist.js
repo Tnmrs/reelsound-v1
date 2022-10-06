ae755fe89282a526f877dbc3a0762189abad2f9e
tnmrs <promomyers@gmail.com>
2022-10-06 20:48:10 +0500

    upd context

--- src/TheMain/Playlist.js
+++ src/TheMain/Playlist.js
@@ -36,14 +36,23 @@ const menuItems = [
 function Playlist({ classes, coverUrl, title, description }) {
   const [isContextMenuOpen, setIsContextMenuOpen] = useState(false);
 
+  const bgClasses = isContextMenuOpen ? 'bg-[#272727]' : 'bg-[#181818] hover:bg-[#272727]';
+
+  /* Открытие меню */
   function openContextMenu(event) {
     event.preventDefault();
     setIsContextMenuOpen(true);
   }
+
+  function closeContextMenu() {
+    console.log('closeContextMenu');
+    setIsContextMenuOpen(false);
+  }
   return (
     <a
       href="/"
-      className={`relative p-4 rounded-md bg-[#181818] hover:bg-[#272727] duration-200 group ${classes}`}
+      className={`relative p-4 rounded-md duration-200 group ${classes} ${bgClasses}`}
+      onClick={(event) => event.preventDefault()}
       onContextMenu={openContextMenu}>
       <div className="relative">
         <PlaylistCover url={coverUrl} />
@@ -55,6 +64,7 @@ function Playlist({ classes, coverUrl, title, description }) {
         <PlaylistContextMenu
           menuItems={menuItems}
           classes="absolute top-9 left-9 bg-[#282828] text-[#eaeaea] text-sm divide-y divide-[#3e3e3e] p-1 rounded shadow-xl cursor-default whitespace-nowrap z-10 "
+          onClose={closeContextMenu}
         />
       )}
     </a>
